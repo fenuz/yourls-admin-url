cc383f5781eb61ebefb668506abdca3fac3f5b1a
Frank Matheron <frankmatheron@gmail.com>
2013-08-29 15:04:19 +0200

    initial commit
    plugin that allows customizing of the yourls admin url

---
+++ plugin.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Cookie Domain 
+Plugin URI: http://www.kennisnet.nl
+Description: Customize Cookie Domain
+Version: 0.1
+Author: Frank Matheron <frankmatheron@gmail.com>
+Author URI: https://github.com/fenuz
+*/
+if (!defined('ADMIN_URL')) {
+    define('ADMIN_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/admin/');
+}
+
+yourls_add_filter('admin_url', 'admin_url_filter');
+function admin_url_filter( $admin, $page ) {
+	$admin = ADMIN_URL . $page;
+	if( yourls_is_ssl() or yourls_needs_ssl() )
+		$admin = str_replace('http://', 'https://', $admin);
+	return $admin
+}
